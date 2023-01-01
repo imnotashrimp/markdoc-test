1652513cf61b8306565681e8bf753a02a926db8a
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2023-01-01 22:08:52 +0200

    get next to render

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const withMarkdoc = require('@markdoc/next.js')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withMarkdoc(nextConfig)({
+  pageExtensions: ['md', 'js', 'jsx']
+});
---
+++ pages/docs/test.md
@@ -0,0 +1,8 @@
+---
+title: Get started with Markdoc
+description: How to get started with Markdoc
+---
+
+# Get started with Markdoc
+
+this is a paragraph
