4e21b70804fbc12986fb73bf48cc311f5616f826
Stefan (Shalom) Boroda <imnotashrimp@gmail.com>
2023-01-02 01:04:29 +0200

    add react-markdown

--- components/Method.jsx
+++ components/Method.jsx
@@ -1,13 +1,21 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
 export const Method = ({frontmatter}) => {
   const {rpcMethod, output} = frontmatter;
 
   const arr = rpcMethod.split(/(?=[A-Z])/g);
+  let response;
 
   switch (output) {
     case 'rest':
-      return <>arr.join(' ')</>;
+       response = arr.join(' ');
+       break;
     case 'velo':
       arr[0] = arr[0].toLowerCase();
-      return <>`${arr.join('')}()`</>;
+      response = `\`${arr.join('')}()\``
+      break;
   }
+
+  return <ReactMarkdown children={response} />
 }
